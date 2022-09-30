a164ebc4baf9cdd925a18212bd0f4f5acbd7fa03
Jaredlll08 <jaredlll08@gmail.com>
2022-09-30 03:34:36 +0200

    Add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,7 +9,10 @@ class MyDocument extends Document {
 
                 <Head>
                     <script async defer data-website-id = "d5a805fd-940e-4c3e-853a-9c5d0dd9febf" src = "https://nlytics.blmj.red/scrpt"/>
-
+                    <link rel = "apple-touch-icon" sizes = "180x180" href = "/apple-touch-icon.png"/>
+                    <link rel = "icon" type = "image/png" sizes = "32x32" href = "/favicon-32x32.png"/>
+                    <link rel = "icon" type = "image/png" sizes = "16x16" href = "/favicon-16x16.png"/>
+                    <link rel = "manifest" href = "/site.webmanifest"/>
                 </Head>
 
                 <body className = {`bg-brand-900`}>

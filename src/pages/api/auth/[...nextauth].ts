c3a5bfcccd87236418d63617b7a116009102da0d
Jared <jaredlll08@gmail.com>
2023-10-06 02:37:52 -0600

    Update github tokens and scope on login

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,11 +1,11 @@
-import NextAuth from "next-auth";
+import NextAuth, {NextAuthOptions} from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export const authOptions = {
+export const authOptions:NextAuthOptions = {
     adapter: PrismaAdapter(prisma),
     secret: process.env.NEXTAUTH_SECRET,
     pages: {
@@ -21,6 +21,24 @@ export const authOptions = {
                 }
             }
         })
-    ]
+    ],
+    callbacks: {
+        async signIn({ user, account, profile, email, credentials }) {
+            await prisma.account.update({
+                data: {
+                    access_token: account.access_token,
+                    token_type: account.token_type,
+                    scope: account.scope
+                },
+                where: {
+                    provider_providerAccountId: {
+                        provider: "github",
+                        providerAccountId: account.providerAccountId
+                    }
+                }
+            })
+            return true
+        },
+    }
 };
 export default NextAuth(authOptions);
